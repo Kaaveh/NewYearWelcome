8c764fe8eadde084b848ba605033dc4b84413fa6
kaaveh <kaaveh@pm.me>
2022-12-04 21:35:36 +0330

    Initial Commit

---
+++ New year welcome/New_year_welcomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  New_year_welcomeApp.swift
+//  New year welcome
+//
+//  Created by Kaaveh Mohamedi on 12/4/22.
+//
+
+import SwiftUI
+
+@main
+struct New_year_welcomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
